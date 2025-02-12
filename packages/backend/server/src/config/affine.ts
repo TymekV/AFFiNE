8d372d909503eda2408d0c75e32e8d725e776b52
Tymek Vonau <vonautymek@gmail.com>
2025-02-12 22:27:58 +0100

    fixes

--- packages/backend/server/src/config/affine.ts
+++ packages/backend/server/src/config/affine.ts
@@ -12,12 +12,12 @@
 // ###############################################################
 //
 // /* The name of AFFiNE Server, may show on the UI */
-AFFiNE.serverName = 'My Selfhosted AFFiNE Cloud';
+AFFiNE.serverName = 'Agin Cloud';
 //
 // /* Whether the server is deployed behind a HTTPS proxied environment */
-AFFiNE.server.https = false;
+AFFiNE.server.https = true;
 // /* Domain of your server that your server will be available at */
-AFFiNE.server.host = 'localhost';
+AFFiNE.server.host = 'affine.tymek.ovh';
 // /* The local port of your server that will listen on */
 AFFiNE.server.port = 3010;
 // /* The sub path of your server */
@@ -34,7 +34,7 @@ AFFiNE.server.port = 3010;
 //
 // /* Whether enable metrics and tracing while running the server */
 // /* The metrics will be available at `http://localhost:9464/metrics` with [Prometheus] format exported */
-// AFFiNE.metrics.enabled = true;
+AFFiNE.metrics.enabled = false;
 //
 //
 // AFFiNE.auth.session = {
@@ -112,55 +112,56 @@ AFFiNE.server.port = 3010;
 // AFFiNE.storages.avatar.provider = 'cloudflare-r2';
 //
 // /* OAuth Plugin */
-// AFFiNE.use('oauth', {
-//   providers: {
-//     github: {
-//       clientId: '',
-//       clientSecret: '',
-//       // See https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
-//       args: {
-//         scope: 'user',
-//       },
-//     },
-//     google: {
-//       clientId: '',
-//       clientSecret: '',
-//       args: {
-//         // See https://developers.google.com/identity/protocols/oauth2
-//         scope: 'openid email profile',
-//         promot: 'select_account',
-//         access_type: 'offline',
-//       },
-//     },
-//     oidc: {
-//       // OpenID Connect
-//       issuer: '',
-//       clientId: '',
-//       clientSecret: '',
-//       args: {
-//         scope: 'openid email profile',
-//         claim_id: 'preferred_username',
-//         claim_email: 'email',
-//         claim_name: 'name',
-//       },
-//     },
-//   },
-// });
+AFFiNE.use('oauth', {
+  providers: {
+    // github: {
+    //   clientId: '',
+    //   clientSecret: '',
+    //   // See https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+    //   args: {
+    //     scope: 'user',
+    //   },
+    // },
+    // google: {
+    //   clientId: '',
+    //   clientSecret: '',
+    //   args: {
+    //     // See https://developers.google.com/identity/protocols/oauth2
+    //     scope: 'openid email profile',
+    //     promot: 'select_account',
+    //     access_type: 'offline',
+    //   },
+    // },
+    oidc: {
+      // OpenID Connect
+      issuer: process.env.OAUTH_OIDC_ISSUER,
+      clientId: process.env.OAUTH_OIDC_CLIENT_ID,
+      clientSecret: process.env.OAUTH_OIDC_CLIENT_SECRET,
+      args: {
+        scope: process.env.OAUTH_OIDC_SCOPE ?? 'openid email profile',
+        claim_id: process.env.OAUTH_OIDC_CLAIM_MAP_USERNAME ?? 'preferred_username',
+        claim_email: process.env.OAUTH_OIDC_CLAIM_MAP_EMAIL ?? 'email',
+        claim_name: process.env.OAUTH_OIDC_CLAIM_MAP_USERNAME ?? 'name',
+      },
+    },
+  },
+});
 //
 // /* Copilot Plugin */
-// AFFiNE.use('copilot', {
-//   openai: {
-//     apiKey: 'your-key',
-//   },
-//   fal: {
-//     apiKey: 'your-key',
-//   },
-//   unsplashKey: 'your-key',
+AFFiNE.use('copilot', {
+  openai: {
+    apiKey: process.env.COPILOT_OPENAI_API_KEY,
+  },
+  fal: {
+    apiKey: process.env.COPILOT_FAL_API_KEY,
+  },
+  unsplashKey: process.env.COPILOT_UNSPLASH_API_KEY,
+  perplexity: process.env.COPILOT_PERPLEXITY_API_KEY,
 //   storage: {
 //     provider: 'cloudflare-r2',
 //     bucket: 'copilot',
 //   }
-// })
+})
 //
 // /* AFFiNE Link Preview & Image Proxy API */
 // AFFiNE.use('worker', {
